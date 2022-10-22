d66ebeb13a74fb3efa5bcdc01252bb64b6d5ded5
boysimple <94022447+boysimple@users.noreply.github.com>
2022-10-23 05:44:33 +0800

    Update LienolOS2.sh

--- LienolOS2.sh
+++ LienolOS2.sh
@@ -20,7 +20,7 @@ sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.$/10.10.$/g' package/base-files/files/bin/config_generate
 sed -i 's/OpenWrt/MightyActionX/g' package/base-files/files/bin/config_generate
 sed -i 's/bootstrap/argonne/g' ./feeds/luci/modules/luci-base/root/etc/config/luci
-rm -rf ./package/diy-ziyong/theme
+
 # rm -rf ./package/diy-ziyong/adguardhome
 rm -rf ./feeds/luci/themes/luci-theme-argon
 rm -rf ./feeds/packages/net/adguardhome
