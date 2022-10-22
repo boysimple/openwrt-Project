21874909c07cb92a483ced702e2daf3ea505756e
boysimple <94022447+boysimple@users.noreply.github.com>
2022-10-23 05:45:29 +0800

    Update LienolOS.sh

--- LienolOS.sh
+++ LienolOS.sh
@@ -16,6 +16,8 @@
 # Add a feed source
 sed -i '$a src-git nas https://github.com/linkease/nas-packages.git;master' feeds.conf.default
 sed -i '$a src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' feeds.conf.default
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 git clone https://github.com/linkease/istore.git package/istore
 git clone https://github.com/linkease/istore-ui.git package/istore-ui
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
